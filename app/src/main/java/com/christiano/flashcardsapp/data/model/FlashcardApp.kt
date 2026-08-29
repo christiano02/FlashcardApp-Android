91f7ada9e4f7f9e7ddaeda4e36d5100e82c6eaba
Christiano02 <christianonascimento02@hotmail.com>
2026-08-29 01:22:29 -0300

    config model

--- app/src/main/java/com/christiano/flashcardsapp/data/model/FlashcardApp.kt
+++ app/src/main/java/com/christiano/flashcardsapp/data/model/FlashcardApp.kt
@@ -1,2 +1,8 @@
 package com.christiano.flashcardsapp.data.model
 
+// O "molde" que define os dados que cada carta vai guardar
+data class FlashcardApp(
+    val trechoIngles: String,
+    val traducaoPTbr: String,
+    val notasPronuncia: String
+)
