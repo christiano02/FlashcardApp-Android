e6afe53b50c3f209990b158ee1a9b9c7f9ad56e1
Christiano02 <christianonascimento02@hotmail.com>
2026-08-31 20:18:57 -0300

    config FlashCardUiState

--- app/src/main/java/com/christiano/flashcardsapp/ui/flashcard/FlashcardUiState.kt
+++ app/src/main/java/com/christiano/flashcardsapp/ui/flashcard/FlashcardUiState.kt
@@ -1,2 +1,14 @@
 package com.christiano.flashcardsapp.ui.flashcard
 
+import com.christiano.flashcardsapp.data.model.FlashcardApp
+
+//O retrato exato de tudo que a tela precisa saber para se desenhar
+data class FlashcardUiState(
+    val listaDeCartoes: List<FlashcardApp> = emptyList(),
+    val indiceAtual: Int = 0,
+    val estaVirado: Boolean = false
+){
+    //uma propriedade facilitadora para gerarmos rapidamente a musica atual
+    val cartaoAtual: FlashcardApp?
+        get() = listaDeCartoes.getOrNull(indiceAtual)
+}
