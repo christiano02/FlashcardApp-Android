2a16da27801244bc68a0f64b73daafed754b8072
Christiano02 <christianonascimento02@hotmail.com>
2026-08-31 20:57:06 -0300

    config CardView

--- app/src/main/java/com/christiano/flashcardsapp/ui/components/CardView.kt
+++ app/src/main/java/com/christiano/flashcardsapp/ui/components/CardView.kt
@@ -1,2 +1,84 @@
 package com.christiano.flashcardsapp.ui.components
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.christiano.flashcardsapp.data.model.FlashcardApp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.*
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+
+
+@Composable
+fun CardView(
+    flashcard: FlashcardApp,
+    estaVirado: Boolean,
+    modifier: Modifier = Modifier
+){
+    ElevatedCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(280.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 6.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            //trecho em ingles sempre visivel
+            Text(
+                text = flashcard.trechoIngles,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+
+            if (estaVirado) {
+                Spacer(modifier = Modifier.height(20.dp))
+
+                HorizontalDivider(
+                    modifier = Modifier.fillMaxSize(0.8f),
+                    thickness = DividerDefaults.Thickness,
+                    color = DividerDefaults.color
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = flashcard.traducaoPTbr,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center,
+                    color = MaterialTheme.colorScheme.primary
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(
+                    text = flashcard.notasPronuncia,
+                    fontSize = 14.sp,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    color = Color.Gray
+                )
+            }
+        }
+    }
+}
